e90eb7b9fd94dff1d2b07c26936e22f78d5d2414
magralol <gnorr_92@hotmail.com>
2014-10-20 19:08:11 +0200

    added a title to doc and made some changes in scipt

--- doc/script.js
+++ doc/script.js
@@ -1,6 +1,6 @@
 $(function(){
     $(".panel").click(function(){
-        $(this).find(".exlanation").css( "display" ) ? $(this).find(".exlanation").show("slow") : $(this).find(".exlanation").hide("slow");
-        //$(this).find(".exlanation").hide("slow")
+        //$(this).find(".exlanation").css( "display" ) ? $(this).find(".exlanation").show("slow") : $(this).find(".exlanation").hide("slow");
+        $(this).find(".exlanation").toggleClass("vissible");
     });
 });
